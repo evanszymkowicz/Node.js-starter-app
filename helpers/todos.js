25884849d958dcf19ca16df444af9eed82b5ea61
Evan Szymkowicz <evan_szymkowicz@Evans-MacBook-Pro.local>
2018-07-27 18:46:53 -0400

    Refacorting Helpers

--- helpers/todos.js
+++ helpers/todos.js
@@ -1,53 +1,57 @@
+var express = require('express');
+var router = express.Router();
 var db = require('../models');
+var helpers = require('../helpers/todos');
+
+router.route('/')
+  .get(helpers.getTodos)
+  .post(helpers.createTodo)
+
+route.get('/:todoId')
 
 exports.getTodos = function(req, res) {
-    db.Todo.find()
-    .then(function(todos){
-        res.json(todos);
-    });
-.catch(function(err){
-        res.send(err);
-    });
-}
-
-exports.createTodo = function(req, res){
-  db.Todo.create(req.body)
-  .then(function(newTodo){
-      res.status(201).json(newTodo);
-  })
+  db.Todo.find().then(function(todos) {
+    res.json(todos);
+  });
   .catch(function(err){
-      res.send(err);
+    res.send(err);
   })
 }
 
-exports.getTodo = function(req, res){
-   db.Todo.findById(req.params.todoId)
-   .then(function(foundTodo){
-       res.json(foundTodo);
-   })
-   .catch(function(err){
-       res.send(err);
-   })
+module.exports = exports;
+
+exports.createTodo = function(req, res) {
+  db.Todo.create(req.body).then(function(newTodo) {
+    res.status(201).json(newTodo);
+  }).catch(function(err) {
+    res.send(err);
+  })
 }
 
-exports.updateTodo =  function(req, res){
-   db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
-   .then(function(todo){
-       res.json(todo);
-   })
-   .catch(function(err){
-       res.send(err);
-   })
+exports.getTodo = function(req, res) {
+  db.Todo.findById(req.params.todoId).then(function(foundTodo) {
+    res.json(foundTodo);
+  }).catch(function(err) {
+    res.send(err);
+  })
 }
 
-exports.deleteTodo = function(req, res){
-   db.Todo.remove({_id: req.params.todoId})
-   .then(function(){
-       res.json({message: 'We deleted it!'});
-   })
-   .catch(function(err){
-       res.send(err);
-   })
+exports.updateTodo = function(req, res) {
+  db.Todo.findOneAndUpdate({
+    _id: req.params.todoId
+  }, req.body, {new: true}).then(function(todo) {
+    res.json(todo);
+  }).catch(function(err) {
+    res.send(err);
+  })
+}
+
+exports.deleteTodo = function(req, res) {
+  db.Todo.remove({_id: req.params.todoId}).then(function() {
+    res.json({message: 'We deleted it!'});
+  }).catch(function(err) {
+    res.send(err);
+  })
 }
 
 module.exports = exports;
