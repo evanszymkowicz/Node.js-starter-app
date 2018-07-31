593952b5996fb14c669344bae17aac58b2964150
Evan Szymkowicz <evan_szymkowicz@Evans-MBP.fios-router.home>
2018-07-30 21:16:05 -0400

    Adding button functionality

--- public/app.js
+++ public/app.js
@@ -1,15 +1,29 @@
 //Request relative path for each user//
-/* $ Dollar sign global variable */
+//$ Dollar sign global variable required for Atom Linter//
 $(document).ready(function () {
   $.getJSON('api/todos');
-  $.then(function (data) {
-    console.log(data);
+  $.then(addTodos);
+  $('#todoInput').keypress(function (event) {
+    if (event.which == 13) {
+      createTodo();
+      console.log('You pressed enter!');
+    }
   });
+});
 
-function addTodos(todos){
-  todos.forEach(function(todo){
+function addTodos(todos) {
+  todos.forEach(function (todo) {
     console.log(todo.name);
-    var newTodo = $('<li class="task">' +todo.name + '</li>');
+    var $newTodo = $('<li class="task">' + todo.name + '</li>');
+    if (todo.completed) {
+      newTodo.addClass('done');
+    }
+
     $('list').append(newTodo);
   });
+}
+
+//Cannot use JSON because we are using a GET request//
+function createTodo() {
+  $.post('api/todos');
 }
